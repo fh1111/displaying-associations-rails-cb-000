d534776a395f70cf1a6342cc719c7e62c830e54f
Fer Hernandez <fh.1111@gmail.com>
2019-01-13 21:33:06 +0000

    Automatically backed up by Learn

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe CategoriesController, type: :controller do
   describe "GET #show" do
     it "returns HTTP success" do
-      get :show,  @category
+      get :show  @category
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #edit" do
     it "returns HTTP success" do
-      get :edit,  @category
+      get :edit  @category
       expect(response).to have_http_status(:success)
     end
   end
